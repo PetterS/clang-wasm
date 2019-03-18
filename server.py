82dd53e2aba1fbcd16aae2b16e31c1bde239b3c6
Petter Strandmark <petter.strandmark@gmail.com>
2019-03-18 13:16:04 +0100

    Add dot to wasm mime type extension.

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import socketserver
 PORT = 4242
 
 Handler = http.server.SimpleHTTPRequestHandler
-Handler.extensions_map["wasm"] = "application/wasm"
+Handler.extensions_map[".wasm"] = "application/wasm"
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("serving at port", PORT)
